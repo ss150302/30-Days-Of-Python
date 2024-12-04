03f5f3e0a5caf91cb078de0a4d66ab30a1850326
ss150302 <sanjay.sisale@gmail.com>
2024-12-04 14:48:18 -0500

    Update search_cdsid_aar_in_daynaprop_namespaces.py

--- 05_Day_Lists/search_cdsid_aar_in_daynaprop_namespaces.py
+++ 05_Day_Lists/search_cdsid_aar_in_daynaprop_namespaces.py
@@ -26,8 +26,7 @@ def search_in_directory(directory, search_terms, file_extensions, exclude_dirs):
 if __name__ == "__main__":
     directory_before = os.getcwd() + '/Before'
     directory_after = os.getcwd() + '/After'
-    #search_terms = ['gjaglan','rsmith46','makdemir','candre36','dbhamra','scharl28','dchira','mciola','jdi1','odonmez1','tertunc','jgartner','cgeyer6','mgoncu','rgorman5','fgradela','agrijal3','ehajzer','ihillawi','aimmig1','gjaglan','ejasso1','sjohn230','vjones1','kkaman','ekuhn7','pledger','pliu29','tmotta3','tnorton2','rmelo25','hozer3','kpadman4','mroman43','wrudin','srygate','spravee4','gsriram4','csteve19','ctsang4','lturton2','ewinsjan','jwebber','rwoodard','yzhan344','hzhou46','yzhu70','szuberi','arose46','plaksh13','skalis1','ihussa25','mfteam','fr_gotd','sbhanda8','ckiefer','swrobel','skalwala','abake113','dwhit277','pkarupai','mgelderm']  # Replace with your comma-separated strings
-    search_terms = ['gjaglan','rsmith46']  # Replace with your comma-separated strings
+    search_terms = ['usr200140','usr100299']  # Replace with your comma-separated strings
 
     file_extensions = ['.xml', '.txt']  # Add the file extensions you want to search
     exclude_dirs = [os.path.join(os.getcwd(), 'Before', 'exclude_dir1'), os.path.join(os.getcwd(), 'After', 'exclude_dir2')]  # Add directories to exclude
