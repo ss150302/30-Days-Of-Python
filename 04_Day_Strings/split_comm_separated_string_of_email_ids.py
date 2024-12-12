51f846f04449c58d6fa2310ef90e4730bc1022ee
ss150302 <sanjay.sisale@gmail.com>
2024-12-12 17:13:54 -0500

    Update split_comm_separated_string_of_email_ids.py

--- 04_Day_Strings/split_comm_separated_string_of_email_ids.py
+++ 04_Day_Strings/split_comm_separated_string_of_email_ids.py
@@ -5,11 +5,34 @@ def split_comma_separated_string(input_string):
     f = StringIO(input_string)
     reader = csv.reader(f, delimiter=',')
     return next(reader)
-
-
-if __name__ == "__main__":
+def use_split_comma_separated_string():
     # Example usage
-    input_string = 'usr1@aaa.com,usr2@bbb.com,'
+    input_string = 'usr1@aaa.com,usr2@bbb.com'
     result = split_comma_separated_string(input_string)
     for item in result:
         print(item)
+
+def use_split_comma_separated_string_in_quote():
+    input_string = 'usr1@aaa.com,usr2@bbb.com'
+    result = split_comma_separated_string(input_string)
+    for item in result:
+        print(f'"{item}"')
+def use_split_and_quote_comma_separated_string():
+    # Example usage
+    input_string = 'usr1@aaa.com,usr2@bbb.com'
+    result = split_and_quote_comma_separated_string(input_string)
+    print(result)  # Output: "usr1@aaa.com","usr2@bbb.com",
+
+def split_and_quote_comma_separated_string(input_string):
+    f = StringIO(input_string)
+    reader = csv.reader(f, delimiter=',')
+    items = next(reader)
+    quoted_items = [f'"{item}"' for item in items]
+    return ','.join(quoted_items)
+
+if __name__ == "__main__":
+    # Example usage
+    use_split_comma_separated_string()
+    use_split_comma_separated_string_in_quote()
+    use_split_and_quote_comma_separated_string()
+
